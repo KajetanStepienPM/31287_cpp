db965285a0905d33e3c0283982665fdfe23438ce
Kajetan Stępień <kajetanstepien@MacBook-Air-Kajetan.local>
2025-04-04 14:45:08 +0200

    Beggining of Dijkstra algorithm implementation

---
+++ src/Djikstra.cpp
@@ -0,0 +1,24 @@
+#include "Graph.h"
+#include <limits>
+#include <queue>
+
+using namespace std;
+
+void Graph::Djikstra(const string& start){
+    unordered_map<string, int> distances;
+    unordered_map<string, bool> visited;
+
+    for(auto& [city, _] : graphData){
+        distances[city] = INT_MAX;
+        visited[city] = false;
+    }
+    distances[start] = 0;
+    
+    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
+    pq.push({0, start});
+
+
+    while(!pq.empty()){
+        auto [dist, city] = pq.top();
+    }
+}
--- src/Graph.h
+++ src/Graph.h
@@ -8,11 +8,12 @@ using namespace std;
 
 class Graph{
     private:
-        unordered_map<string, vector<pair<string, int> > > graphData;
+        unordered_map<string, vector<pair<string, int>>> graphData;
     public:
         void addGraphEdge(const string& city1, const string& city2, int distance); //wskazniki, poniewaz uzywam nazw miast zamiast np. ich ID
         void printGraph(); //funkcja, ktora docelowo bedzie drukowala cala liste sasiedztwa dla miasta
-        vector<pair<string, int> > getNeighbours(const string& city){
+        void Djikstra(const string& start);
+        vector<pair<string, int>> getNeighbours(const string& city) {
             return graphData[city];
         }
-};
+        };
